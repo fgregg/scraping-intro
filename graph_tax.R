53561cbb0e36e14540c5b57645581f8fd8a1ce49
Forest Gregg <fgregg@uchicago.edu>
2012-11-01 12:03:47 -0500

    renamed readme to org and updated graph file

--- graph_tax.R
+++ graph_tax.R
@@ -1,9 +1,25 @@
 taxes <- read.csv("taxes.csv", header=TRUE)
 
-chicago_tax <- taxes[taxes$tax_type == "CHMR",]
+chicago_tax <- taxes[taxes$tax_type == "HMR",]
 
-chicago_total <- ts(chicago_tax$total, c(1999,3), c(2012,2), 4)
-chicago_payers <- ts(chicago_tax$number_taxpayers, c(1999,3), c(2012,2), )
+chicago_total <- aggregate(chicago_tax$total,
+                           by=list(paste(chicago_tax$year,
+                             chicago_tax$quarter)),
+                           FUN=sum,
+                           na.rm=TRUE)
 
-plot(chicago_total/1000000, ylab="Dollars, millions", main="Chicago Home Rule Revenue by Quarter")
+chicago_payers <- aggregate(chicago_tax$number_taxpayers,
+                            by=list(paste(chicago_tax$year,
+                              chicago_tax$quarter)),
+                            FUN=sum,
+                            na.rm=TRUE)
+
+chicago_total <- ts(chicago_total$x, c(1999,3), c(2012,2), 4)
+chicago_payers <- ts(chicago_payers$x, c(1999,3), c(2012,2), 4)
+
+tax_rate = c(rep(.01, 24), rep(.0125, 28))
+
+plot((chicago_total/tax_rate)/1000000000, ylab="Dollars, billions", main="Taxable Sales")
+
+plot(chicago_payers)
 
