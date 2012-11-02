b42242841774d12e271d8994f2eeb64e98615e09
Forest Gregg <fgregg@uchicago.edu>
2012-11-02 08:56:08 -0500

    graphs

--- graph_tax.R
+++ graph_tax.R
@@ -1,9 +1,31 @@
+library(cairoDevice)
+
 taxes <- read.csv("taxes.csv", header=TRUE)
 
-chicago_tax <- taxes[taxes$tax_type == "CHMR",]
+chicago_rate = c(rep(.01, 24), rep(.0125, 28))
 
-chicago_total <- ts(chicago_tax$total, c(1999,3), c(2012,2), 4)
-chicago_payers <- ts(chicago_tax$number_taxpayers, c(1999,3), c(2012,2), )
+chicago_tax <- taxes[taxes$tax_type == "HMR",]
 
-plot(chicago_total/1000000, ylab="Dollars, millions", main="Chicago Home Rule Revenue by Quarter")
+chicago_total <- aggregate(chicago_tax$total,
+                           by=list(paste(chicago_tax$year,
+                             chicago_tax$quarter)),
+                           FUN=sum)
 
+chicago_total <- ts(chicago_total$x, c(1999,3), c(2012,2), 4)
+chicago_total <- chicago_total/chicago_rate
+
+
+png('sales.png')
+plot(chicago_total/1000000000, ylab="Dollars, billions", main="Taxable Sales in Chicago")
+dev.off()
+
+chicago_payers <- aggregate(chicago_tax$number_taxpayers,
+                            by=list(paste(chicago_tax$year,
+                              chicago_tax$quarter)),
+                            FUN=sum)
+
+chicago_payers <- ts(chicago_payers$x, c(1999,3), c(2012,2), 4)
+
+png('retailers.png')
+plot(chicago_payers, main="Retailers in Chicago")
+dev.off()
